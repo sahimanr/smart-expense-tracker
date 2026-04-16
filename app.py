f6c2851df6cda8937aa494684cfde6512b74aae2
sahimanr <raisahiman3@gmail.com>
2026-04-16 13:28:23 +0530

    Update app.py

--- app.py
+++ app.py
@@ -12,6 +12,8 @@ app=Flask(__name__)
 app.secret_key = os.environ.get("SECRET_KEY")
 app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///database.db"
 db= SQLAlchemy(app)
+with app.app_context():
+        db.create_all()
 
 login_manager=LoginManager()
 login_manager.init_app(app)
@@ -150,6 +152,4 @@ def delete_expense(expense_id):
     return redirect(url_for('dashboard'))
 
 if __name__=="__main__":
-    with app.app_context():
-        db.create_all()
     app.run(debug=True)
